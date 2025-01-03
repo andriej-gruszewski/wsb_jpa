ce805b91ad8021787026f43f83f947e2634929dc
a878624 <patryk.wonsewicz@atos.net>
2025-01-03 17:21:53 +0100

    mappers,T0, Dao impl

---
+++ wsb-jpa1/src/main/java/com/jpacourse/persistence/dao/PatientDao.java
@@ -0,0 +1,10 @@
+package com.jpacourse.persistence.dao;
+
+import com.jpacourse.persistence.entity.PatientEntity;
+import com.jpacourse.persistence.entity.VisitEntity;
+
+import java.time.LocalDateTime;
+
+public interface PatientDao extends Dao<PatientEntity, Long> {
+    VisitEntity addVisitToPatient(Long patientId, Long doctorId, LocalDateTime visitTime, String description);
+}
