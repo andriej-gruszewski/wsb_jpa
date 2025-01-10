720397d2a248ff9ae8a2d20c26e52a188fd6b715
a878624 <patryk.wonsewicz@atos.net>
2025-01-10 18:24:21 +0100

    patientserviceimpl

---
+++ wsb-jpa1/src/main/java/com/jpacourse/service/impl/PatientServiceImpl.java
@@ -0,0 +1,62 @@
+package com.jpacourse.service.impl;
+
+import com.jpacourse.dto.PatientTO;
+import com.jpacourse.mapper.PatientMapper;
+import com.jpacourse.persistence.dao.PatientDao;
+import com.jpacourse.persistence.dao.VisitDao;
+import com.jpacourse.persistence.entity.PatientEntity;
+import com.jpacourse.persistence.entity.VisitEntity;
+import com.jpacourse.service.PatientService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class PatientServiceImpl implements PatientService {
+
+    private final PatientDao patientDao;
+    private final VisitDao visitDao;
+
+    @Autowired
+    public PatientServiceImpl(PatientDao patientDao, VisitDao visitDao) {
+        this.patientDao = patientDao;
+        this.visitDao = visitDao;
+    }
+
+    @Override
+    @Transactional
+    public void deletePatient(Long id) {
+        patientDao.delete(id);
+    }
+
+    @Override
+    public List<VisitEntity> findAllVisitsForPatient(Long patientId) {
+        return visitDao.findByPatientId(patientId);
+    }
+
+    @Override
+    @Transactional
+    public PatientTO findById(Long id) {
+        PatientEntity entity = patientDao.findOne(id);
+        return PatientMapper.mapToTO(entity);
+    }
+
+    public List<PatientTO> findAll() {
+        return patientDao.findAll()
+                .stream()
+                .map(PatientMapper::mapToTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public PatientTO savePatient(PatientTO patientTO) {
+        PatientEntity entity = PatientMapper.mapToEntity(patientTO);
+        PatientEntity savedEntity = patientDao.save(entity);
+        return PatientMapper.mapToTO(savedEntity);
+    }
+}
